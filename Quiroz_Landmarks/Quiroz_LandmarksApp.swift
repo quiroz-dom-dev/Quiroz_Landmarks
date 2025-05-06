e0e32e90516f857e4bce9496feedc157a5708041
quiroz-dom-dev <quirozd@merrimack.edu>
2025-05-06 10:48:27 -0400

    Following Step 4. Updating main as the entry point for the app.

--- Quiroz_Landmarks/Quiroz_LandmarksApp.swift
+++ Quiroz_Landmarks/Quiroz_LandmarksApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct Quiroz_LandmarksApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
