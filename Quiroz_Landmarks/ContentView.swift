afc4fc24eb01841934035a0906aca83e4d80a032
quiroz-dom-dev <quirozd@merrimack.edu>
2025-05-06 12:39:50 -0400

    Adding Structure and combining map, image, and details.

--- Quiroz_Landmarks/ContentView.swift
+++ Quiroz_Landmarks/ContentView.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading){
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            Quiroz_MapView()
+                .frame(height: 300)
+            
+            Quiroz_CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+        
+            VStack(alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive Text Goes Here")
             }
-        }
         .padding()
+        Spacer()
+        }
     }
 }
 
